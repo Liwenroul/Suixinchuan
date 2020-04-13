523818fc694a4b59fbde07617c06d271b5a74934
siyuannnnn-hsy <1587935941@qq.com>
2020-04-13 17:01:17 +0800

    siyuan

---
+++ Sxc-front/MyDemo/App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React,{useState,useEffect}  from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import SplashScreen from 'react-native-splash-screen'
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App = () => {
+  
+  return (
+    <View>
+      <Text>aaa</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
