4ab8010624548d6387958800e5ba203b5bedd60f
Liwenroul <2728608191@qq.com>
2020-05-25 20:33:15 +0800

    0525-2033

--- Sxc-front/myAppN/src/home/Home.js
+++ Sxc-front/myAppN/src/home/Home.js
@@ -57,37 +57,16 @@ export default class Home extends Component {
         }
     }
     componentDidMount(){
-        fetch('http://192.168.0.106:3000/sale')
+        fetch('http://192.168.2.102:3000/sale')
             .then(res=>res.json())
             .then(res=>{
                 this.setState({tits: res});
             })
-        fetch('http://192.168.0.106:3000/merchandise')
+        fetch('http://192.168.2.102:3000/merchandise')
             .then(res=>res.json())
             .then(res=>{
                 this.setState({tit: res});
             })
-        // fetch('https://github.com/facebook/react-native/user',
-        // {
-        //     method:'GET', 
-        //     headers:{ 'Accept':'application/json', 'Content-Type':'application/json', } 
-        // })
-        //     .then(res=>res.json())
-        //     .then(json =>{ console.log(json); this.setState({ tits: json }) 
-        //     // .then(res=>{
-        //         // console.log(res);
-        //         // this.setState({tits: res});
-        //     })
-    //     fetch('https://facebook.github.io/react-native/movies.json')
-    //   .then((response) => response.json())
-    //   .then((responseJson) => {
-    //     console.log(responseJson.movies);
-    //     this.setState({tits: responseJson.movies});
-    //     return responseJson.movies;
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
     }
     // componentDidUpdate(){
     //     fetch('http://192.168.0.106:3000/user')
@@ -122,54 +101,8 @@ export default class Home extends Component {
                 backgroundColor: '#fff'
             }}>
 
-                 {/* <StatusBar backgroundColor="red" /> */}
-                 {/* <View style={{
-                    width: '100%',
-                    height: 70 * s,
-                    borderColor: '#eee',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                }}> */}
-                     {/* <TouchableOpacity style={{ width: '15%', alignItems: 'center' }}>
-                         <Icon
-                            name="bars"
-                            style={{
-                                color: '#ccc', fontSize: 25
-                            }}
-                        />
-                    </TouchableOpacity> */}
-                    {/* <View style={{
-                        marginTop: 10 * s,
-                        marginBottom: 10 * s,
-                        width: '80%',
-                        height: 50 * s,
-                        flexDirection: 'row',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        borderRadius: 25,
-                        backgroundColor: '#eee',
-                        opacity: 0.75,
-                        paddingLeft: 10
-                    }}> */}
-                        {/* <TouchableOpacity>
-                            <Icon
-                                name="search"
-                                style={{ color: 'gray', fontSize: 20 }}
-                            />
-                        </TouchableOpacity> */}
-                        {/* <TextInput placeholderTextColor="gray"
-                            placeholder="音乐会晚礼服"
-                            style={{
-                                width: '86%',
-                                height: 40,
-                                color: '#fff'
-                            }}
-                        /> */}
-                    {/* </View> */}
-                {/* </View> */}
-
                 <ScrollView>
-                    {/* <ScrollView
+                    <ScrollView
                         pagingEnabled={true}
                         horizontal={true}
                         style={{ height: 300 * s, width: 640 * s }}
@@ -186,7 +119,7 @@ export default class Home extends Component {
                         <View style={styles.slide}>
                             <Image style={{width:'100%',height:'100%'}} resizeMode='stretch' source={require('../../assets/raw_1528737314.png')} />
                         </View>
-                    </ScrollView> */}
+                    </ScrollView>
                     <ScrollView
                         pagingEnabled={true}
                         horizontal={true}
@@ -212,161 +145,62 @@ export default class Home extends Component {
                                 ))
                         }   
                        
-                        {/* <View style={{
-                            width: 190 * s,
-                            height: 200 * s,
-                            alignItems: 'center',
-                            justifyContent:'center'
-                        }}>
-                            <TouchableOpacity style={styles.btn}  onPress={()=>Actions.detail()}>
-                                <Text>实用券攻略</Text>
-                                <Text style={{fontSize:10}}>礼盒券后直减130</Text>
-                                <View style={styles.img}>
-                                    <Image style={{width:'30%',height:'100%'}} resizeMode='stretch' source={require('../../assets/v2_q5kktg.jpg')}/>
-                                    <Image  style={{width:'45%',height:'50%'}} resizeMode='stretch' source={require('../../assets/v2_q5kkuw.jpg')}/>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{
-                            width: 190 * s,
-                            height: 200 * s,
-                            alignItems: 'center',
-                            justifyContent:'center'
-                        }}>
-                            <TouchableOpacity style={styles.btn} onPress={()=>Actions.detail()}>
-                                <Text>神价热销榜</Text>
-                                <Text style={{fontSize:10}}>礼盒券后直减130</Text>
-                                <View style={styles.img}>
-                                <Image style={{width:'90%',height:'30%',marginTop:100*s}} resizeMode='stretch' source={require('../../assets/v2_q5kkvl.jpg')}/>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{
-                            width: 190 * s,
-                            height: 200 * s,
-                            alignItems: 'center',
-                            justifyContent:'center'
-                        }}>
-                            <TouchableOpacity style={styles.btn} onPress={()=>Actions.detail()}>
-                                <Text>包包特价</Text>
-                                <Text style={{fontSize:10}}>礼盒券后直减130</Text>
-                                <View style={styles.img}>
-                                    <Image style={{width:'45%',height:'85%'}} resizeMode='stretch' source={require('../../assets/v2_q5kl1a.jpg')}/>
-                                    <Image  style={{width:'35%',height:'75%',marginLeft:10*s}} resizeMode='stretch' source={require('../../assets/v2_q5kl1u.jpg')}/>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{
-                            width: 190 * s,
-                            height: 200 * s,
-                            alignItems: 'center',
-                            justifyContent:'center'
-                        }}>
-                            <TouchableOpacity style={styles.btn}  onPress={()=>Actions.detail()}>
-                                <Text>防晒美白榜</Text>
-                                <Text style={{fontSize:10}}>礼盒券后直减130</Text>
-                                <View style={styles.img}>
-                                    <Image style={{width:'20%',height:'90%'}} resizeMode='stretch' source={require('../../assets/raw_1521984443.png')}/>
-                                    <Image  style={{width:'20%',height:'90%',marginLeft:20*s}} resizeMode='stretch' source={require('../../assets/raw_1521984479.png')}/>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{
-                            width: 190 * s,
-                            height: 200 * s,
-                            alignItems: 'center',
-                            justifyContent:'center'
-                        }}>
-                            <TouchableOpacity style={styles.btn}  onPress={()=>Actions.detail()}>
-                                <Text>人气面膜榜</Text>
-                                <Text style={{fontSize:10}}>礼盒券后直减130</Text>
-                                <View style={styles.img}>
-                                    <Image style={{width:'35%',height:'70%'}} resizeMode='stretch' source={require('../../assets/raw_1521984866.png')}/>
-                                    <Image  style={{width:'36%',height:'85%',marginLeft:10*s}} resizeMode='stretch' source={require('../../assets/raw_1521984570.png')}/>
-                                </View>
-                            </TouchableOpacity>
-                        </View> */}
-                        {/* <View style={{
-                            width: 190 * s,
-                            height: 200 * s,
-                            alignItems: 'center',
-                            justifyContent:'center'
-                        }}>
-                            <TouchableOpacity style={styles.btn}  onPress={()=>Actions.detail()}>
-                                <Text>查看全部</Text>
-                                <Text style={{fontSize:10}}>See more</Text>
-                            </TouchableOpacity>
-                        </View> */}
+                    </ScrollView>
                 </ScrollView>
-                </ScrollView>
+                <ScrollView>
                     {
                         this.state.tit.map((item,key)=>(
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', flexWrap: 'wrap', }}>
-                    
-                        <View style={{
-                            width: '45%',
-                            height: 500 * s,
-                            alignItems: 'center',
-                            justifyContent:'center',
-                            marginTop: 20*s,
-                            padding:4,
-                            backgroundColor:'#eee'
-                        }}>
-<<<<<<< Updated upstream
-                            <TouchableOpacity style={{width:'100%',height:'90%'}} onPress={()=>Actions.detail({'merid':'m123456'})}>
-                                <Image  style={{width:'100%',height:'90%',borderRadius:20}} resizeMode='stretch' source={require('../../assets/v2_q5klar.jpg')} />
-                                <Text style={{marginTop:15*s}}>{item.tit.slice(8,)}</Text>
-                                {/* <Text style={{marginTop:8*s}}>{item.shopbrand}</Text> */}
-=======
-                            <TouchableOpacity style={{width:'100%',height:'90%'}} onPress={()=>Actions.detail()}>
-                                <Image  style={{width:'100%',height:'90%',borderRadius:20}} resizeMode='stretch' source={require('../../assets/v2_q5klar.jpg')} />
-                                <Text style={{marginTop:15*s}}>{item.tit.slice(8,)}</Text>
-                                {/* <Text style={{marginTop:8*s}}>{item.shopbrand}</Text> */}
-                            <TouchableOpacity style={{width:'100%',height:'90%'}} onPress={()=>Actions.detail({'merid':'m123456'})}>
-                                <Image  style={{width:'100%',height:'85%',borderRadius:20}} resizeMode='stretch' source={require('../../assets/v2_q5klar.jpg')} />
-                                <Text style={{marginTop:8*s}}>酒红色蕾丝优雅长款礼裙</Text>
-                                <Text style={{marginTop:8*s}}>BLANCHE</Text>
->>>>>>> Stashed changes
-                            </TouchableOpacity>
-                            <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',width:'100%',height:'10%'}}>
-                                <Text style={{marginTop:5*s}}>{item.shopbrand}</Text>
-                                <Text style={{color:'red',}}>￥{item.price}/4日</Text>
-                                {/* <TouchableOpacity  onPress={this.shoucang}>
-                                    <Icon name='heart-o' ref="changeColor" style={{fontSize:25}}/>
-                                </TouchableOpacity> */}
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', flexWrap: 'wrap', }}>
+                            <View style={{
+                                width: '45%',
+                                height: 500 * s,
+                                alignItems: 'center',
+                                justifyContent:'center',
+                                marginTop: 20*s,
+                                padding:4,
+                                backgroundColor:'#eee'
+                            }}>
+                                <TouchableOpacity style={{width:'100%',height:'90%'}} onPress={()=>Actions.detail({'merid':item.merid})}>
+                                    <Image  style={{width:'100%',height:'90%',borderRadius:20}} resizeMode='stretch' source={require('../../assets/v2_q5klar.jpg')} />
+                                    <Text style={{marginTop:15*s}}>{item.tit.slice(8,)}</Text>
+                                    {/* <Text style={{marginTop:8*s}}>{item.shopbrand}</Text> */}
+                                </TouchableOpacity>
+                                <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',width:'100%',height:'10%'}}>
+                                    <Text style={{marginTop:5*s}}>{item.shopbrand}</Text>
+                                    <Text style={{color:'red',}}>￥{item.price}/4日</Text>
+                                    {/* <TouchableOpacity  onPress={this.shoucang}>
+                                        <Icon name='heart-o' ref="changeColor" style={{fontSize:25}}/>
+                                    </TouchableOpacity> */}
+                                </View>
                             </View>
-                        ))
-                        {/* </View> */}
                         
-                        <View style={{
-                            width: '45%',
-                            height: 500 * s,
-                            alignItems: 'center',
-                            marginTop: 20*s,
-                            padding:4,
-                            backgroundColor:'#eee'
-                        }}>
-                            <TouchableOpacity style={{width:'100%',height:'90%'}}>
-                                <Image   style={{width:'100%',height:'90%',borderRadius:20}} resizeMode='stretch' source={require('../../assets/v2_q5klq0.jpg')} />
-                                <Text style={{marginTop:15*s}}>宝石蓝抹胸晚礼服</Text>
-                                {/* <Text style={{marginTop:8*s}}>BLANCHE</Text> */}
-                            </TouchableOpacity>
-                            <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',width:'100%',height:'10%'}}>
+                            <View style={{
+                                width: '45%',
+                                height: 500 * s,
+                                alignItems: 'center',
+                                marginTop: 20*s,
+                                padding:4,
+                                backgroundColor:'#eee'
+                            }}>
+                                <TouchableOpacity style={{width:'100%',height:'90%'}}>
+                                    <Image   style={{width:'100%',height:'90%',borderRadius:20}} resizeMode='stretch' source={require('../../assets/v2_q5klq0.jpg')} />
+                                    <Text style={{marginTop:15*s}}>宝石蓝抹胸晚礼服</Text>
+                                    {/* <Text style={{marginTop:8*s}}>BLANCHE</Text> */}
+                                </TouchableOpacity>
+                                <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',width:'100%',height:'10%'}}>
                                 
-                                <Text style={{marginTop:5*s}}>BLANCHE</Text>
-                                <Text style={{color:'red'}}>￥399/4日</Text>
+                                    <Text style={{marginTop:5*s}}>BLANCHE</Text>
+                                    <Text style={{color:'red'}}>￥399/4日</Text>
                                 {/* <TouchableOpacity  >
                                     <Icon name='heart-o' style={{fontSize:25,}}/>
                                 </TouchableOpacity> */}
+                                </View>
                             </View>
                         </View>
-                        
-                    </View>
-                    )
-                }
-                 
+                        ))
+                    }
                 </ScrollView>
-                </View>
+             </View>
 
 
 
